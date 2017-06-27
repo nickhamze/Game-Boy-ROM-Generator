f1e8e20467842fee317eebdc0cc140ab3156c1f6
Nick Hamze <me@nickhamze.com>
2017-06-26 23:19:02 -0500

    turn off dither

--- rombuilder/pygbconv.py
+++ rombuilder/pygbconv.py
@@ -47,7 +47,7 @@ def convimg(name):
         exit(1)
 
     # Convert the image to RGB
-    img = img.convert('RGB')
+    img = img.convert('mode=RGB,dither=None')
     pixels = img.getdata()
     colors = []
     hex_colors = []
