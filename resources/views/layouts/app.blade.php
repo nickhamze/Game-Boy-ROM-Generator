3133d1d6585199bdfacf7a9f7786cf61d97f48a8
Nick Hamze <me@nickhamze.com>
2017-07-06 21:14:40 -0500

    Update CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <title>Game Boy ROM Generator by Yokoi</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/appv3.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/appv4.css') }}" rel="stylesheet">
     <link href="{{ asset('css/noty.css') }}" rel="stylesheet">
 </head>
 <body>
